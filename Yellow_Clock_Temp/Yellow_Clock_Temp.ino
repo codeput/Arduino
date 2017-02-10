3545e3ebc7ecd0e1c9ea3cb6d9a765b304637ee9
codeput <emaxduck@gmail.com>
2017-02-10 12:53:47 +0100

    Update Yellow_Clock_Temp.ino

--- Yellow_Clock_Temp/Yellow_Clock_Temp.ino
+++ Yellow_Clock_Temp/Yellow_Clock_Temp.ino
@@ -3,27 +3,14 @@
 RTC_DS1307 RTC;
 #include <dsp7s04b.h>
 #include "DHT.h"
-
-#define DHTPIN 2     // what digital pin we're connected to
-unsigned long ti;
 // Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
-
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define DHTPIN 2     // what digital pin we're connected to DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 char buffer[10];
+unsigned long ti;
+
 void setup()
 {
   Serial.begin(9600);
@@ -32,7 +19,6 @@ void setup()
   dsp7s04b.setBrightness(200);
   RTC.begin();
   dht.begin();
-
 }
 
 void loop () {
